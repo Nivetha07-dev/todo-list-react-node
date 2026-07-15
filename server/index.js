a723729b7d42c25a1ae4a58fe7f73eb315ce8908
Nivetha07-dev <nivethavelmurugan17@gmail.com>
2026-07-15 15:39:18 +0200

    server port number changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const tasksRouter = require('./routes/tasks');
 const userRouter = require('./routes/user');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
